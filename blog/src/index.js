285191e23d96a6a41be8e85cc5cf2eec300d49a9
yakshjethva <43207315+yakshjethva@users.noreply.github.com>
2022-06-21 05:12:05 +0000

    comment added

--- blog/src/index.js
+++ blog/src/index.js
@@ -19,6 +19,7 @@ root.render(
 	</>
 );
 
+/* Elements must be inside any tag (empty tag are also allowed <></>)*/
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
